980fada7ce674361d72cfe7867361b568781b21e
彭海燕 <1824817338@qq.com>
2021-09-12 09:50:24 +0800

    完成了分类参数的开发

--- src/router/index.js
+++ src/router/index.js
@@ -7,44 +7,27 @@ import Users from '../components/user/User.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
-
+import Params from '../components/goods/Params.vue'
 Vue.use(VueRouter)
 
 const routes = [
   // 路由重定向
-  {
-    path: '/',
-    redirect: '/login'
-  },
-  {
-    path: '/login',
-    component: Login
-  },
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
   {
     path: '/home',
     component: Home,
     redirect: '/welcome',
     children: [
-      {
-        path: '/welcome',
-        component: Welcome
-      },
-      {
-        path:'/users',
-        component: Users
-      },
-      {
-        path:'/rights',
-        component:Rights
-      },
-      {
-        path:'/roles',
-        component:Roles
-      },
-      {path:'/categories',component:Cate}
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles },
+      { path: '/categories', component: Cate },
+      { path: '/params', component: Params }
     ]
   },
-  
+
 ]
 
 const router = new VueRouter({
